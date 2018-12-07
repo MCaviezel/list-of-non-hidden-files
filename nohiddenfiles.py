c8110be78c80dcb6c208f00cbed41ff91fdeaec1
Marco Caviezel <marcocaviezel@Marcos-MacBook-Pro.local>
2018-12-07 12:16:12 +0100

    Updated version

--- nohiddenfiles.py
+++ nohiddenfiles.py
@@ -1,9 +1,17 @@
-@author: marcocaviezel
-
+"""@author: marcocaviezel
+"""
 import os
 import re
 
-def nohiddenfiles(path = os.listdir()):
+def ignorehiddenfiles(path = os.listdir()):
+"""
+Ignore hidden files \n
+This is a handy function to get the list of all variables in a folder, without hidden files (starting with a dot, e.g. .DS_store).\n
+Use \n
+clean_list_of_pwd = nohiddenfiles()\n
+to get the list of the pwd (as default). \n
+Alternatively you can enter a path to another directory.
+"""
     import os
     import re
     regex = re.compile(r'^\.')
